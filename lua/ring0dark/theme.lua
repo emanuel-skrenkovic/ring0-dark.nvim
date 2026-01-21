8465a0538669736fe451becc9fec3d504db1489a
Emanuel Skrenković <eskrenkovic@gmail.com>
2026-01-21 21:01:07 +0100

    cowardly

--- lua/ring0dark/theme.lua
+++ lua/ring0dark/theme.lua
@@ -12,27 +12,27 @@ theme.set_highlights = function()
 	hl(0, "NormalSB", { fg = c.fg, bg = c.bg_darker }) -- Normal text in side bar
 	hl(0, "Pmenu", { fg = c.fg, bg = c.bg_darker })
 	hl(0, "SignColumn", { fg = "NONE", bg = c.bg })
-	hl(0, "CursorLineNr", { fg = c.yellow, bg = "NONE", bold = true })
-	hl(0, "Statement", { fg = c.yellow, bg = "NONE", bold = true })
+	hl(0, "CursorLineNr", { fg = c.yellow, bg = "NONE" })
+	hl(0, "Statement", { fg = c.yellow, bg = "NONE" })
 	hl(0, "TelescopeSelection", { fg = c.fg, bg = c.gray })
 	hl(0, "CursorLine", { fg = "NONE", bg = c.gray })
 	hl(0, "StatusLine", { fg = c.fg, bg = c.gray })
 
 	-- special words
 	hl(0, "Comment", { fg = c.brown, bg = "NONE" })
-	hl(0, "Special", { fg = c.fg, bg = "NONE", bold = true })
+	hl(0, "Special", { fg = c.fg, bg = "NONE" })
 	hl(0, "@variable", { fg = c.fg, bg = "NONE" })
 	hl(0, "Constant", { fg = c.fg, bg = "NONE" })
 	hl(0, "Function", { fg = c.fg, bg = "NONE" })
 	hl(0, "String", { fg = c.green, bg = "NONE" })
 	hl(0, "Identifier", { fg = c.fg, bg = "NONE" })
-	hl(0, "Title", { fg = c.yellow, bg = "NONE", bold = true })
-	hl(0, "Type", { fg = c.light_gray, bg = "NONE", bold = true })
+	hl(0, "Title", { fg = c.yellow, bg = "NONE" })
+	hl(0, "Type", { fg = c.light_gray, bg = "NONE" })
 
 	-- diagnostics
-	hl(0, "DiagnosticError", { fg = c.red, bg = "NONE", bold = true })
-	hl(0, "DiagnosticWarn", { fg = c.yellow, bg = "NONE", bold = true })
-	hl(0, "DiagnosticHint", { fg = c.cyan, bg = "NONE", bold = true })
+	hl(0, "DiagnosticError", { fg = c.red, bg = "NONE" })
+	hl(0, "DiagnosticWarn", { fg = c.yellow, bg = "NONE" })
+	hl(0, "DiagnosticHint", { fg = c.cyan, bg = "NONE" })
 end
 
 return theme
